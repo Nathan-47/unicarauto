2c66c7ccac2d56f24300e9d175ca6f664efe8d28
Nathan-47 <nathan.parchment47@gmail.com>
2026-08-11 21:33:32 +0100

    feat: add Navbar component to RootLayout for improved navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import Navbar from './components/navbar'
 
 const manrope = Manrope({
   display: "swap",
@@ -22,7 +23,10 @@ export default function RootLayout({
       lang="en"
       className={`${manrope.className} min-h-full flex flex-col`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
