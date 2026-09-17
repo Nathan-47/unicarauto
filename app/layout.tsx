a806d11c3818ba6bac268d3e89b5b6ceba4c3060
Nathan-47 <nathan.parchment47@gmail.com>
2026-09-17 21:47:18 +0100

    feat: added footer component to improve layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import Navbar from './components/navbar'
+import Footer from "./components/footer";
 
 const manrope = Manrope({
   display: "swap",
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
+        <Footer />
         </body>
     </html>
   );
