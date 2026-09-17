c67e887e12c38760a0bd5d3692eda065607ec87b
Nathan-47 <nathan.parchment47@gmail.com>
2026-09-17 21:50:49 +0100

    feat: Features component displayed and copy amended

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,61 @@
-import Image from "next/image";
+import Link from 'next/link'
+import features from './components/features'
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
+    <main>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+
+              <div className="mx-auto max-w-2xl py-8 sm:py-8 lg:py-20">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+                <div className="text-center">
+                  <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
+                    Cars that make sense for students.
+                  </h1>
+                  <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+                    Are you a student or young person and cant get your head around
+                    cars? No sweat here at Unicar we’ve got you covered explore the
+                    site and we guarantee you’ll be more car ready  than ever before.
+                  </p>
+                  <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <Link
+                      href="/cars"
+                      className="rounded-md bg-green-500 px-3.5 py-2.5 text-2xl text-white font-bold shadow-xs hover:bg-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
+                    >
+                      Find your car
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              </div>
+      
+            {/* how it works section */}
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-20">
+              <div className="mx-auto max-w-2xl lg:text-center">
+                <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
+                  How it works
+                </h2>
+              </div>
+              <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                  {features.map((feature) => (
+                    <div key={feature.name} className="relative pl-16">
+                      <dt className="text-base/7 font-semibold text-gray-900">
+                        <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-gray-900">
+                          <feature.icon
+                            aria-hidden="true"
+                            className="size-6 text-white"
+                          />
+                        </div>
+                        {feature.name}
+                      </dt>
+                      <dd className="mt-2 text-base/7 text-gray-600">
+                        {feature.description}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+    </main>
+  )};
