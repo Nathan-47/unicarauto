df36613dc495263a605766bdfa71ebf7370c5d36
Nathan-47 <nathan.parchment47@gmail.com>
2026-09-17 21:48:41 +0100

    feat: Amended features component content

---
+++ app/components/features.tsx
@@ -0,0 +1,31 @@
+import { MagnifyingGlassIcon, CpuChipIcon, HeartIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/outline'
+
+const features = [
+  {
+    name: 'Find your car',
+    description:
+      'Search for the car that meets your needs.',
+    icon: MagnifyingGlassIcon,
+  },
+  {
+    name: 'Maintenance trouble',
+    description:
+      'Need help on how to fix parts or maintain your car, you are in the right place.',
+    icon: WrenchScrewdriverIcon,
+  },
+  {
+    name: 'LLM help',
+    description:
+      'AI insight to help you pick the best car available via reliability scores',
+    icon: CpuChipIcon,
+  },
+  {
+    name: 'Final check',
+    description:
+      'Once all is checked use all data given to buy your car, Happy Hunting!',
+    icon: HeartIcon,
+  },
+]
+
+export default features;
+
